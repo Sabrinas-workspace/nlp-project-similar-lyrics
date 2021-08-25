ad091347c8a84a7d48133377a30c0a5b14616c6c
Sabrinas-workspace <88004840+Sabrinas-workspace@users.noreply.github.com>
2021-08-25 13:49:37 +0200

    added 5 nouns/ topics functions to SongtextTopics.py

--- nlp-project/SongtextTopics.py
+++ nlp-project/SongtextTopics.py
@@ -1,5 +1,34 @@
 # Python Module SongtextTopics
 
-def find_main_topics(a):
-    topics = []
+import spacy
+from collections import Counter
+
+nlp = spacy.load(("en_core_web_sm"))
+
+def get_nouns(songtext):
+    doc = nlp(songtext.lower())
+    all_nouns = [token.lemma_ for token in doc if token.pos_ == "NOUN"]
+    return all_nouns
+
+def nouns_sorted(songtext):
+    nouns = get_nouns(songtext)
+    sorted_nouns = Counter(nouns)
+    return sorted_nouns
+    
+def find_topics(songtext):
+    nouns = nouns_sorted(songtext)
+    topics = [key for key, value  in nouns.most_common() if value > 1]
     return topics
+
+def find_fewer_topics(songtext):
+    nouns = nouns_sorted(songtext)
+    fewer_topics = [key for key, value  in nouns.most_common() if value > 2]
+    return fewer_topics
+
+def find_main_topics(songtext):
+    sorted_topics = find_topics(songtext)
+    if len(sorted_topics) <= 5:
+        main_topics = sorted_topics
+    else:
+        main_topics = find_fewer_topics(songtext)
+    return main_topics
