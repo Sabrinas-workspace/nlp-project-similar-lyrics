eac22d4755782a0d72e16d62712ec4a6a234d6b3
Sabrinas-workspace <88004840+Sabrinas-workspace@users.noreply.github.com>
2021-08-30 10:52:32 +0200

    SongtextSentiment,py added similarity function

--- nlp-project/SongtextSentiment.py
+++ nlp-project/SongtextSentiment.py
@@ -19,7 +19,7 @@ def song_polarity_lines(songtext):
         polarity_analysis += TextBlob(line).polarity
         count += 1
     result = polarity_analysis/count
-    return polarity_analysis
+    return result
 
 def song_subjectivity(songtext):
     subjectivity_analysis = TextBlob(songtext).subjectivity
@@ -34,3 +34,43 @@ def pos_neg_neutral_polarity(songtext, root):
     else:
         result = "The polarity of '" + SongInformation.get_songtitle(songtext, root) + "' by " + SongInformation.get_artist(songtext, root) + " is neutral"
     return result
+
+def similar_mood_polarity(songtitle, artist, root):
+    for child in root:
+        if SongInformation.get_songtitle_child(child) == songtitle and SongInformation.get_artist_child(child) == artist:
+            songtext = child.find("songtext").text
+            song_child = child
+    polarity = song_polarity_lines(songtext)
+
+    minimum = 10.0
+    result = ""
+
+    for child in root:
+        if child != song_child:
+            songtext_child = SongInformation.get_songtext_child(child)
+            polarity_child = song_polarity_lines(songtext_child)
+    
+            
+            if polarity > 0 and polarity_child > 0:
+                difference = polarity - polarity_child
+                if abs(difference) < minimum:
+                    minimum = abs(difference)
+                    minimum_song = child
+                    result = "'" + SongInformation.get_songtitle_child(minimum_song) + "' by " + SongInformation.get_artist_child(minimum_song) + " has a similiar mood to '" + songtitle + "' by " + artist
+
+            if polarity < 0 and polarity_child < 0:
+                difference = abs(polarity - polarity_child)
+                if abs(difference) < minimum:
+                    minimum = abs(difference)
+                    minimum_song = child
+                    result = "'" + SongInformation.get_songtitle_child(minimum_song) + "' by " + SongInformation.get_artist_child(minimum_song) + " has a similiar mood to '" + songtitle + "' by " + artist
+                    
+            
+
+    return result
+
+    
+
+
+        
+
