5d6bfabf96fb8a21360d05bf56138fd402aaa1cc
Sabrinas-workspace <88004840+Sabrinas-workspace@users.noreply.github.com>
2021-08-30 10:51:43 +0200

    Update SongInformation.py

--- nlp-project/SongInformation.py
+++ nlp-project/SongInformation.py
@@ -2,19 +2,35 @@ import xml.etree.ElementTree as ET
 
 from multimethod import multimethod
 
-def get_songtext(child):
+def get_songtext_child(child):
     songtext = child.find("songtext").text
     return songtext
 
+def get_songtext(songtitle, artist, root):
+    for child in root:
+        artist_child = child.find("artist")
+        if "".join(child.attrib.values()) == songtitle and "".join(artist_child.attrib.values()) == artist:
+            songtext = child.find("songtext").text
+    return songtext
+
+def get_songtitle_child(child):
+    songtitle = "".join(child.attrib.values())
+    return songtitle
+
 def get_songtitle(songtext, root):
     for child in root:
-        if get_songtext(child) == songtext:
+        if get_songtext_child(child) == songtext:
             songtitle = "".join(child.attrib.values())
     return songtitle
 
+def get_artist_child(child):
+    artist_child = child.find("artist")
+    artist = "".join(artist_child.attrib.values())
+    return artist
+
 def get_artist(songtext, root):
     for child in root:
-        if get_songtext(child) == songtext:
+        if get_songtext_child(child) == songtext:
             artist_child = child.find("artist")
             artist = "".join(artist_child.attrib.values())
     return artist
