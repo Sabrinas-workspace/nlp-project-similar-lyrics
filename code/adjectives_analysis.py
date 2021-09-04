1846fb1f5dbf80e88310ab83970eb50ef69dffe3
Sabrinas-workspace <88004840+Sabrinas-workspace@users.noreply.github.com>
2021-09-04 22:36:10 +0200

    Create adjectives_analysis.py

---
+++ code/adjectives_analysis.py
@@ -0,0 +1,45 @@
+"""This module tests the module songtext_adjectives and exports the results.
+"""
+
+import xml.etree.ElementTree as ET
+import pandas as pd
+import songtext_adjectives
+import song_information
+
+# Insert the path to your XML corpus and comment in the next line
+# tree = ET.parse('PATH')
+root = tree.getroot()
+
+# Some example queries
+print(songtext_adjectives.query_get_song_recommendation("Demons", "Imagine Dragons", root))
+print("")
+print(songtext_adjectives.query_get_song_recommendation("Time", "NF", root))
+print("")
+print(songtext_adjectives.query_get_song_recommendation("Bad at love", "Halsey", root))
+print("")
+
+
+# Creates dataframe for results
+songtitles = []
+artists = []
+similar_adjectives = []
+for child in root:
+    songtitle = song_information.get_songtitle(child)
+    artist = song_information.get_artist(child)
+    similar_songs = songtext_adjectives.find_similar_songs(child, root)
+    songtitles.append(songtitle)
+    artists.append(artist)
+    similar_adjectives.append(similar_songs)
+
+songs = {'Songtitle': songtitles,
+         'Artist': artists,
+         'Songs with similar adjectives': similar_adjectives
+        }
+
+df = pd.DataFrame(songs, columns = ['Songtitle', 'Artist',
+                    'Songs with similar adjectives'])
+
+# Insert the path to the folder in which you want to save the results file
+# and comment in the next line
+# df.to_csv (r'PATH/adjectives_results.csv', sep= ';', index = False, header=True)
+print(df)
